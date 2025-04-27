a678bb2af02ca500d3d62c4bf870df5efb13faaa
Gfachini02 <gabrielfachinirp@gmail.com>
2025-04-26 23:15:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 #include <string.h>
 #include "funcoes.h"
 
-
-
-int main (){
-
+int main() {
     int natureza = 0, cultural = 0, festivo = 0, relaxante = 0;
+    Cidades* listaCidades = NULL;
+
+    // carrega os dados das cidades e atrações
+    carregarDados(&listaCidades);
 
     // questionario para experiencia personalizada
     aplicarQuestionario(&natureza, &cultural, &festivo, &relaxante);
-    
-    // classifica as atrações de acordo com o gosto pessoal do usuario
-    aplicarPontuacaoNasAtracoes(atracoesInicio, natureza, cultural, festivo, relaxante);
+
+    // aplica pontuação nas atrações de cada cidade
+    Cidades* cidadeAtual = listaCidades;
+    while (cidadeAtual != NULL) {
+        aplicarPontuacaoNasAtracoes(cidadeAtual->atracao, natureza, cultural, festivo, relaxante);
+        cidadeAtual = cidadeAtual->prox;
+    }
 
     return 0;
 }
