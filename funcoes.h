b8357bcc6b7f069acac9568f8f56cbc712d26ee6
Gfachini02 <gabrielfachinirp@gmail.com>
2025-04-26 23:34:12 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -21,7 +21,12 @@ typedef struct cidades{
     struct cidades *prox;
     struct cidades *ant;
 } Cidades;
-
+//na funcao linkarAtracaoCidade, tem esse tipo, porem n foi criado ent eu criei pra compilar.
+typedef struct cidadeAtracao {
+    struct cidades* cidade;
+    struct atracoes* atracao;
+    struct cidadeAtracao* prox;
+} CidadeAtracao;
 // Informações sobre a viagem do usuário
 typedef struct viagem {
     Cidades* cidade;
@@ -36,7 +41,8 @@ typedef struct descritor {
 
 //carrega dados do arquivo cidades-atracoes.txt
 void carregarDados(Cidades **listaCidades);
-
+//lista as cidades carregadas
+void listarCidadesComAtracoes(Cidades *lista);
 // funções que personalizam a experiencia do usuario
 void aplicarQuestionario(int *natureza, int *cultural, int *festivo, int *relaxante);
 void aplicarPontuacaoNasAtracoes(Atracoes *lista, int natureza, int cultural, int festivo, int relaxante);
